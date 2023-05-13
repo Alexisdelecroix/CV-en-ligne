66910f6c6e678cfda6cc69292e31ace9b8046e61
Delecroix <d-alexis@hotmail.fr>
2023-05-13 18:12:20 +0200

    W3C

--- app.js
+++ app.js
@@ -2,14 +2,6 @@ let sidenav = document.getElementById("myAside");
 let openn = document.getElementById("open");
 let closeBtn = document.getElementById("closeBtn");
 
-
-
-/* Set the width of the side navigation to 250px */
-
-
-/* Set the width of the side navigation to 0 */
-
-
 openn.onclick = function openNav() {
     sidenav.classList.add("active");
   }
