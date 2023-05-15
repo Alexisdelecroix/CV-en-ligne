8d776d0c39595107c79b48f0ee11302662a3c86a
Delecroix <d-alexis@hotmail.fr>
2023-05-15 17:55:54 +0200

    Ouverture et Fermeture de la popup

--- app.js
+++ app.js
@@ -9,4 +9,17 @@ closeBtn.onclick = function closeNav() {
     sidenav.classList.remove("active");
   }
 
+
+  
+let detail = document.getElementById("overlay");
+let opennDetail = document.getElementById("openDetail");
+let closeeDetail = document.getElementById("closeDetail");
+
+opennDetail.onclick = function openNav() {
+    detail.classList.add("active");
+  }
+closeeDetail.onclick = function closeNav() {
+    detail.classList.remove("active");
+  }
+
   
