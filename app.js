f53a9281ccbde06be8e94605919ef1b39e1cb659
Delecroix <d-alexis@hotmail.fr>
2023-05-21 21:25:58 +0200

    Mode dark terminer, plus passage du w3c validator

--- app.js
+++ app.js
@@ -33,11 +33,17 @@ let titre = document.getElementsByTagName("h3");
 let titrewhite = document.getElementsByTagName("h3");
 let intru = document.getElementById("intru");
 let sousTitreBleu = document.getElementsByTagName("h4");
+let cercle = document.getElementsByClassName("cercle");
+let line = document.getElementsByClassName("block");
+let overlayBox = document.getElementById("overlayBox");
 
 modeMoon.onclick = function moon() {
   corpsDePage.classList.add("active");
   modeSun.classList.add("dark");
   intru.classList.add("titreDark");
+  opennDetail.classList.add("active");
+  overlayBox.classList.add("active");
+
   for (let i = 0; i < para.length; i++) {
     para[i].style.color = "white";
   }
@@ -58,12 +64,20 @@ modeMoon.onclick = function moon() {
   for (let i = 0; i < titreDeveloppeur.length; i++) {
     titreDeveloppeur[i].style.color = "#65BAFB";
   }
+  for (let i = 0; i < cercle.length; i++) {
+    cercle[i].style.background = "white";
+  }
+  for (let i = 0; i < line.length; i++) {
+    line[i].style.borderLeft = "3px solid white";
+  }
 };
 
 modeSun.onclick = function sun() {
   corpsDePage.classList.remove("active");
   modeSun.classList.remove("dark");
   intru.classList.remove("titreDark");
+  opennDetail.classList.remove("active");
+  overlayBox.classList.remove("active");
   for (let i = 0; i < para.length; i++) {
     para[i].style.color = "black";
   }
@@ -83,5 +97,12 @@ modeSun.onclick = function sun() {
   }
   for (let i = 0; i < titreDeveloppeur.length; i++) {
     titreDeveloppeur[i].style.color = "#537188";
+  }
+  for (let i = 0; i < cercle.length; i++) {
+    cercle[i].style.background = "black";
+  }
+
+  for (let i = 0; i < line.length; i++) {
+    line[i].style.borderLeft = "3px solid black";
   }
 };
